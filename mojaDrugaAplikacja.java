caf808c65dd14428d923b454b9132e0123cf5fbf
Margoroshek59 <margoroshek00@gmail.com>
2025-03-13 19:27:47 +0200

    moja druga praca w rerozutorium Lab2

--- mojaDrugaAplikacja.java
+++ mojaDrugaAplikacja.java
@@ -6,46 +6,34 @@ public class mojaDrugaAplikacja {
     public static void main(String[] args) {
 
            //to jest komentarz liniowy
-       /* Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         /*
         To jest komentarz blokowy
          moze on miec wiele linii
          tak, jak tytaj
           */
-        /*System.out.println("Prosze podaj swoje imie:");
-        // nie wiem czy musze podawac swoje imie
-        String forename = scanner.nextLine();
+        System.out.println("Prosze podaj swoj wiek:");
+        double sekond = scanner.nextDouble();
+        double s = 31557600;
 
-        System.out.println("Prosze podaj swoje nazwisko:");
-        String surname = scanner.nextLine();
+        System.out.println("twoj wiek w sekondach: " + (s * sekond));
 
-        scanner.close();
-
-        System.out.println("Witaj " + forename + " " + surname) ;
-
-         */
-
-        double x = 10; //utworzenie zmiennej typu double i przypisanie jej wartosci 10
+        /* double x = 10; //utworzenie zmiennej typu double i przypisanie jej wartosci 10
         double y = 2;
 
 
         Scanner scanner = new Scanner(System.in);
 
-        var result = x + y;
-        System.out.println("x + y = "  + result);
-
-        result = x - y;
-        System.out.println("x - y = " + result);
+        var result = x ;
+        System.out.println("x ");
 
         result = x * y;
         System.out.println("x * y = " + result);
 
-        result = x / y;
-        System.out.println("x / y = " + result);
+        result = x * x;
+        System.out.println("x * x = " + result);
 
-        result = x % y;
-        System.out.println("x % y = " + result);
 
         System.out.println("Wpisz dwie liczby rozdzielone klawiszem Enter: ");
 
@@ -54,6 +42,10 @@ public class mojaDrugaAplikacja {
 
         System.out.println("x + y = " + (first + second));
 
+         */
+
 
     }
 }
+
+
